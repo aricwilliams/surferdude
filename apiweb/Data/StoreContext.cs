63ad52f7bcb475e63588f102ed32e4ed6db4a325
aric williams <aricwilliamst@gmail.com>
2022-11-02 19:43:16 -0400

    Add project files.

---
+++ apiweb/Data/StoreContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using apiweb.Entities;
+using Microsoft.AspNetCore.Identity;
+using System.Data.Entity;
+
+namespace apiweb.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
